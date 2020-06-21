6970e5d9d6868b31d7153251c2be769a3f833a12
koru1130 <me@koru.me>
2020-06-22 02:30:58 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple multilateration for WGS84 (latitude, longitude)
+## Usage:
+```language=python
+from multilateration import multilateration
+
+# ((latitude,longitude), distance(in km))
+datas = [
+((25.0664615,121.5549009), 2.23),
+((25.0336862,121.5648103), 3.14),
+((25.0462432,121.5174745), 2.38)
+]
+
+print(multilateration(datas))
+```
---
+++ multilateration.py
@@ -0,0 +1,13 @@
+from geopy import distance
+from scipy.optimize import least_squares
+import numpy as np
+
+def residuals_fn(points):
+    def fn(location):        
+        return np.array( [ distance.distance(p, location).km - r for (p, r) in points ] )
+    return fn
+
+def multilateration(points):
+    ps = [x[0] for x in points] 
+    x0 = np.mean(np.array(ps),axis=0)
+    return least_squares(residuals_fn(points), x0).x
