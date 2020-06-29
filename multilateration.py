a4d2806b602a00454f28936a285d30a02acb6056
koru1130 <koru1130@gmail.com>
2020-06-29 11:38:08 +0800

    change to weight by square of distance

--- multilateration.py
+++ multilateration.py
@@ -4,10 +4,10 @@ import numpy as np
 
 def residuals_fn(points):
     def fn(location):        
-        return np.array( [ (distance.distance(p, location).km - r)/(r*r) for (p, r) in points ] )
+        return np.array( [ (distance.distance(p, location).km - r)*(r*r) for (p, r) in points ] )
     return fn
 
 def multilateration(points):
     ps = [x[0] for x in points] 
     x0 = np.mean(np.array(ps),axis=0)
-    return least_squares(residuals_fn(points), x0)
+    return least_squares(residuals_fn(points), x0).x
