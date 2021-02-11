375087d6c7bda972bff6aba6922c0e4845778425
koru1130 <koru1130@users.noreply.github.com>
2021-02-11 12:19:38 +0000

    support great_circle distance

--- multilateration.py
+++ multilateration.py
@@ -2,12 +2,19 @@ from geopy import distance
 from scipy.optimize import least_squares
 import numpy as np
 
-def residuals_fn(points):
+def residuals_fn(points, dist):
     def fn(location):        
-        return np.array( [ (distance.distance(p, location).km - r)*(r*r) for (p, r) in points ] )
+        return np.array( [ (dist(p, location).km - r)*(r*r) for (p, r) in points ] )
     return fn
 
-def multilateration(points):
+def multilateration(points, dist_type ='geodesic'):
+    if dist_type == 'geodesic' :
+        dist = distance.distance
+    elif dist_type == 'great_circle' :
+        dist = distance.great_circle
+    else:
+        raise Exception("dist_type error")
+
     ps = [x[0] for x in points] 
     x0 = np.mean(np.array(ps),axis=0)
-    return least_squares(residuals_fn(points), x0).x
+    return least_squares(residuals_fn(points, dist), x0).x
